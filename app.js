f6c2081de41a654447df38d4bc9fefd6fea201c1
Viktor Borszéki <vborszeki@gmail.com>
2017-12-06 10:14:43 +0100

    fix code style

--- app.js
+++ app.js
@@ -14,27 +14,27 @@ for (let i = 0; i < 230; i ++) {
   const geometry = new THREE.CylinderGeometry( 34, 34, 168, 50, 50 );
   let texture = new THREE.TextureLoader().load('beer.png');
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-  texture.repeat.set( 1, 1 );
+  texture.repeat.set(1, 1);
 
-  const material = new THREE.MeshBasicMaterial({map: texture});
+  const material = new THREE.MeshBasicMaterial({ map: texture });
   cylinder = new THREE.Mesh(geometry, material);
   cylinder.position.x = Math.random() * 1000 - 500;
-	cylinder.position.y = Math.random() * 1000 - 500;
-	cylinder.position.z = Math.random() * 1000 - 500;
-	cylinder.rotation.x = Math.random() * 2 * Math.PI;
-	cylinder.rotation.y = Math.random() * 2 * Math.PI;
-	cylinder.rotation.z = Math.random() * 2 * Math.PI;
-  scene.add( cylinder );
+  cylinder.position.y = Math.random() * 1000 - 500;
+  cylinder.position.z = Math.random() * 1000 - 500;
+  cylinder.rotation.x = Math.random() * 2 * Math.PI;
+  cylinder.rotation.y = Math.random() * 2 * Math.PI;
+  cylinder.rotation.z = Math.random() * 2 * Math.PI;
+  scene.add(cylinder);
 }
 
 camera.position.z = 500;
 
 function animate() {
-	requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
   camera.position.z = Math.sin(Date.now() * 0.002) * 500;
-	camera.position.y = Math.sin(Date.now() * 0.002) * 300;
-	camera.lookAt(cylinder.position);
-	renderer.render(scene, camera);
+  camera.position.y = Math.sin(Date.now() * 0.002) * 300;
+  camera.lookAt(cylinder.position);
+  renderer.render(scene, camera);
 }
 
 animate();
